ab30fef3af759ec2b0844543cf458338bb906dfd
whejhe <dominguezalacid@gmail.com>
2024-03-07 14:40:32 +0100

    actualizar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { HeaderComponent } from './components/header/header/header.component';
+import { FooterComponent } from "./components/footer/footer.component";
+import { SideBarComponent } from "./components/side-bar/side-bar.component";
 
 @Component({
     selector: 'app-root',
@@ -9,9 +11,11 @@ import { HeaderComponent } from './components/header/header/header.component';
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     imports: [
-      RouterOutlet,
-      NavBarComponent,
-      HeaderComponent
+        RouterOutlet,
+        NavBarComponent,
+        HeaderComponent,
+        FooterComponent,
+        SideBarComponent
     ]
 })
 export class AppComponent {
