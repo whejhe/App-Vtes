2d29cfdcdb174c89701cba1da16af84198d71efe
Carlos Fernandez <dominguezalacid@gmail.com>
2024-03-07 21:01:10 +0100

    actualizar

---
+++ src/app/pages/main/custom/custom.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom.component.html',
+  styleUrl: './custom.component.scss'
+})
+export class CustomComponent {
+
+}
