fcc8971d3a221007cd32c7f183fef8743cb67c9f
Carlos Fernandez <dominguezalacid@gmail.com>
2024-03-09 11:30:08 +0100

    actualizar

---
+++ src/app/models/diseño-cards.model.ts
@@ -0,0 +1,12 @@
+export interface diseñoCardVampire{
+  nombre: string;
+  tipo: ImageData;
+  coste:ImageData;
+  imagen:ImageData;
+  description: string;
+  clan: ImageData;
+  disciplinas: ImageData[];
+  grupo:string;
+  edicicion:ImageData;
+
+}
