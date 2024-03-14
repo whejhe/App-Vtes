6b8e2adf3f2a7e3a58376eb570fb83d8177bdb2d
Carlos Fernandez <dominguezalacid@gmail.com>
2024-03-14 08:17:25 +0100

    actualizar

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,3 +11,7 @@ app.listen(app.get("port"), () => {
     console.log("Servidor corriendo en el puerto: ", app.get("port"));
     console.log("En el modo: ", process.env.NODE_ENV);
 })
+
+app.get('/data', (req, res) => {
+    res.send('Datos desde backend');
+})
