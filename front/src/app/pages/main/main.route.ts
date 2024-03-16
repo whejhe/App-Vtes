f6da0a223f27aebef2c21efcfb7369c84fb23cca
Carlos Fernandez <dominguezalacid@gmail.com>
2024-03-16 08:22:09 +0100

    save

--- front/src/app/pages/main/main.route.ts
+++ front/src/app/pages/main/main.route.ts
@@ -5,11 +5,13 @@ import { CriptaComponent } from "./cripta/cripta.component";
 import { InicioComponent } from "./inicio/inicio.component";
 import { CustomComponent } from "./custom/custom.component";
 import { EventsComponent } from "./events/events.component";
+import { PruebasComponent } from "./pruebas/pruebas.component";
 
 export const MAIN_ROUTE:Routes = [
   {path: 'inicio', component: InicioComponent},
   {path: 'cripta', component: CriptaComponent},
   {path: 'biblioteca', component: BibliotecaComponent},
   {path: 'custom', component: CustomComponent},
-  {path: 'events', component: EventsComponent}
+  {path: 'events', component: EventsComponent},
+  {path: 'pruebas', component: PruebasComponent}
 ]
