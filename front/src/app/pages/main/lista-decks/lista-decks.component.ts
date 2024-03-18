166e0d8156b30cfad52d3236e34bff2094a46967
whejhe <dominguezalacid@gmail.com>
2024-03-18 13:17:50 +0100

    añadiendo imagenes a la galeria

--- front/src/app/pages/main/lista-decks/lista-decks.component.ts
+++ front/src/app/pages/main/lista-decks/lista-decks.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { FichaDeckComponent } from "../ficha-deck/ficha-deck.component";
 
 @Component({
-  selector: 'app-lista-decks',
-  standalone: true,
-  imports: [],
-  templateUrl: './lista-decks.component.html',
-  styleUrl: './lista-decks.component.scss'
+    selector: 'app-lista-decks',
+    standalone: true,
+    templateUrl: './lista-decks.component.html',
+    styleUrl: './lista-decks.component.scss',
+    imports: [FichaDeckComponent]
 })
 export class ListaDecksComponent {
 
--- front/src/app/pages/main/main.route.ts
+++ front/src/app/pages/main/main.route.ts
@@ -3,15 +3,23 @@ import { Routes } from "@angular/router";
 import { BibliotecaComponent } from "./biblioteca/biblioteca.component";
 import { CriptaComponent } from "./cripta/cripta.component";
 import { InicioComponent } from "./inicio/inicio.component";
-import { CustomComponent } from "./custom/custom.component";
 import { EventsComponent } from "./events/events.component";
 import { FichaDeckComponent } from "./ficha-deck/ficha-deck.component";
+import { ListaDecksComponent } from "./lista-decks/lista-decks.component";
+import { ListarEventosComponent } from "./listar-eventos/listar-eventos.component";
+import { PortadaCustomCardComponent } from "./portada-custom-card/portada-custom-card.component";
+import { CustomComponent } from "./custom-cards/custom/custom.component";
+import { GalleryComponent } from "./custom-cards/gallery/gallery.component";
 
 export const MAIN_ROUTE:Routes = [
   {path: '', component: InicioComponent},
   {path: 'cripta', component: CriptaComponent},
   {path: 'biblioteca', component: BibliotecaComponent},
+  {path: 'portada-custom-card', component: PortadaCustomCardComponent},
   {path: 'custom', component: CustomComponent},
+  {path: 'gallery', component: GalleryComponent},
   {path: 'events', component: EventsComponent},
-  {path: 'ficha-deck', component: FichaDeckComponent}
+  {path: 'ficha-deck', component: FichaDeckComponent},
+  {path: 'lista-decks', component: ListaDecksComponent},
+  {path: 'listar-eventos', component: ListarEventosComponent},
 ]
